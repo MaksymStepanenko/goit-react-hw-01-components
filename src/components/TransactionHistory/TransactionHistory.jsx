994e458c3d6c2db8e6610dee3d5ba7a8f1d78927
MaksymStepanenko <113137299+MaksymStepanenko@users.noreply.github.com>
2023-04-06 12:22:12 +0300

    5

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import css from './TransactionHistory.module.css';
+// import css from './TransactionHistory.module.css';
 import TransactionLineItem from './TransactionLineItem';
 
 export default function TransactionHistory({ data }) {
--- src/components/TransactionHistory/TransactionLineItem.jsx
+++ src/components/TransactionHistory/TransactionLineItem.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import css from './TransactionHistory.module.css';
+// import css from './TransactionHistory.module.css';
 
 export default function TransactionLineItem({ type, amount, currency }) {
   return (
